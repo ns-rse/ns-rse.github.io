fa6f262154357b4e0d6e68ecd70be4d6dc6e104f
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-10-26 10:08:17 +0100

    Adding sample.py for talk

---
+++ posts/pre-commit/sample.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+
+def find_and_load_files(file_path: Union[str, Path], file_type: str) -> List:
+    """Recursively find and load files of the stated type along the given file path."""
+    # We have a really long comment on this line just for demonstration purposes so that we can generate a few errors that need linting
+    try:
+        return Path(file_path).rglob(f"*{file_type}")
+    except:
+        raise
