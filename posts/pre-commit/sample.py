b384ae61ac1c6eb3966a1cd65243e6927a42d465
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-11-15 10:35:47 +0000

    Updates to slides and resources

--- posts/pre-commit/sample.py
+++ posts/pre-commit/sample.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pathlib import Path
 
 
-def find_and_load_files(file_path: Union[str, Path], file_type: str) -> List:
+def find_and_load_files(file_path: Union[str, Path], file_type: str):
     """Recursively find and load files of the stated type along the given file path."""
     # We have a really long comment on this line just for demonstration purposes so that we can generate a few errors that need linting
     try:
